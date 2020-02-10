5db63cf0d6b3b0a6c9fd1d3824415e1c30307db8
Michael Corey <mikejcorey@gmail.com>
2020-02-10 15:35:51 -0600

    Removed more unused stuff

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
 	import Map from './Map.svelte';
 	import VoteDensityMap from './VoteDensityMap.svelte';
 	import Autocomplete from './Autocomplete.svelte';
-	import County from './County.svelte';
 
 	import nh from './data/nh.json';
 	import nh_cities from './data/nh_cities.json';
@@ -236,7 +235,3 @@
 	<iframe title="National delegate count [draft]" aria-label="Interactive line chart" id="datawrapper-chart-X8NB4" src="//datawrapper.dwcdn.net/X8NB4/2/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="450"></iframe>
 	<script type="text/javascript">!function(){"use strict";window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}})}();</script>
 </section>
-
-<!-- {#each county_data_grouped as county}
-	<County {county}/>
-{/each} -->
