9a480d94cb40c278f11dd1409543a128fadb4455
Michael Corey <mikejcorey@gmail.com>
2020-02-25 13:10:54 -0600

    whoops missing component

---
+++ src/LastUpdated.svelte
@@ -0,0 +1,14 @@
+<script>
+
+  export let timestamp;
+  let last_updated;
+  let options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'};
+
+  $: {
+    let datestring = new Date(timestamp)
+    last_updated = datestring.toLocaleString('en-US', options)
+  }
+
+</script>
+
+<p class="lastUpdated">Last change: <span class="updatedTime">{last_updated}</span></p>
